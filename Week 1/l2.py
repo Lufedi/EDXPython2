f8c31a4a154ebc1532e4db86fd64c663cbe45898
luisfediaz <pipexir@gmail.com>
2015-11-18 20:18:34 -0500

    PS3

---
+++ Week 1/l2.py
@@ -0,0 +1,13 @@
+__author__ = 'luisfediaz'
+import random
+
+
+random.seed(9001)
+d = random.randint(1, 10)
+for i in xrange(random.randint(1, 10)):
+    if random.randint(1, 10) < 7:
+        print d
+    else:
+        random.seed(9001)
+        d = random.randint(1, 10)
+        print random.randint(1, 10)
---
+++ Week 1/test.py
@@ -0,0 +1,23 @@
+__author__ = 'luisfediaz'
+
+
+
+
+import pylab
+
+
+# Second Example (comment the above code before uncommiting this example)
+
+principal = 10000 #initial investment
+interestRate = 0.05
+years = 20
+values = []
+for i in range(years + 1):
+    values.append(principal)
+    principal += principal*interestRate
+pylab.plot(range(years+1), values)
+pylab.title('5% Growth, Compounded Annually')
+pylab.xlabel('Years of Compounding')
+pylab.ylabel('Value of Principal ($)')
+pylab.show()
+
